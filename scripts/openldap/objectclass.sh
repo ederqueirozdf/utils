d8ab98e61aa60631ece5e00f7f807e751affaeab
Eder Queiroz <ederbrito.df@gmail.com>
2019-07-08 16:14:17 -0300

    Update objectclass.sh

--- scripts/openldap/objectclass.sh
+++ scripts/openldap/objectclass.sh
@@ -6,6 +6,6 @@ do
     echo $linha >> objectclass.ldif
     echo "changetype: modify" >> objectclass.ldif
     echo "add: objectClass" >> objectclass.ldif
-    echo "objectClass: organizationalMMA" >> objectclass.ldif
+    echo "objectClass: organizationalNOME" >> objectclass.ldif
     echo "" >> objectclass.ldif
 done < $FILE
