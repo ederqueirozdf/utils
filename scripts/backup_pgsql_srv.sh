e9bd106ca7035c806d0da54d31c6313c8578b706
Eder Queiroz <ederbrito.df@gmail.com>
2019-07-23 17:16:16 -0300

    Rename backup_pgsql_srv43.sh to backup_pgsql_srv.sh

--- scripts/backup_pgsql_srv.sh
+++ scripts/backup_pgsql_srv.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 
 DIA=`date +%Y-%m-%d-%H%M%S`
-DIA_DEL=`date -v-7d +%Y-%m-%d`                 # Quantidade de dias que os arquivos ser„o guardados
+DIA_DEL=`date -v-7d +%Y-%m-%d`                 # Quantidade de dias que os arquivos ser√£o guardados
 
 # Retorna todos os databases existentes
 DATABASES=`/usr/local/bin/psql --tuples-only -U pgsql postgres -c "select datname from pg_catalog.pg_database"`
