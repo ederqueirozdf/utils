92ced09cb7566a9ac3baa2e7df71f327247d454b
Eder Queiroz <ederbrito.df@gmail.com>
2019-07-23 17:17:15 -0300

    Update addatributos.sh

--- scripts/openldap/addatributos.sh
+++ scripts/openldap/addatributos.sh
@@ -3,4 +3,4 @@ sed '/uid/a gidnumber: 500\
 objectclass: inetOrgPerson\
 objectclass: posixAccount\
 objectclass: top\
-userpassword: {MD5}fvA7yxL/+fUIgKPxwTvZuQ==' ASIN.ldif >> ASIN-FINAL.ldif
+userpassword: {MD5}fvA7yxL/+fUIgKPxwTvZuQ==' nomeLDIF.ldif >> NOMELDIF-FINAL.ldif
